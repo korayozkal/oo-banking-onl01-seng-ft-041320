c700445d39d995063aba0df7043ce4af23ddbbf1
Koray Ozkal <koryozkal@gmail.com>
2020-05-03 00:21:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,4 +9,24 @@ def initialize(name)
   @balance = 1000
   
 end
+
+def balance
+  @balance
+end
+def status
+  @status
+end
+def deposit(amount)
+  @balance += amount
+end
+def display_balance
+  "Your balance is $#{@balance}."
+end
+def valid?
+  balance > 0 && status == "open"
+end
+def close_account
+   self.status = "closed"
+ end
+
 end
