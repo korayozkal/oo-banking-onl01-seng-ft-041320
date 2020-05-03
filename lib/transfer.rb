d6089953b48c3cc3a833212c30f5183de5ca0824
Koray Ozkal <koryozkal@gmail.com>
2020-05-03 00:15:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+ attr_accessor :sender, :receiver, :status, :amount
+
+ 
+def initialize(sender)
+ @sender= sender
+ @receiver= receiver
+ @status= "pendind"
+ 
+ 
 end
